16082b8b27e421962c60e12962e77ce2a1a145c0
Irox1 <86012238+Irox1@users.noreply.github.com>
2021-06-17 22:30:21 +0200

    Update volumeconepyra.py

--- volumeconepyra.py
+++ volumeconepyra.py
@@ -1,1 +1,19 @@
+import math
+corp = str(input("Veuillez entré Cônes ou Pyramide: "))
+mesure = str(input("Entrer la mesure: "))
 
+
+if corp == "Cônes":
+    n1 = int(input("Entrer le rayon du Cônes: "))
+    n = int(input("Entrer la hauteur du Cônes:  "))
+    n2 = pow(n1,2)*math.pi
+    n3 = n2*n/3
+    print("{} {}".format(n3, mesure))
+    
+
+
+elif corp == "Pyramides":
+    n1 = int(input("Entrer un coté de la base de la Pyramide: "))
+    n = int(input("Entrer la hauteur de la Pyramide: "))
+    n2 = pow(n1,2)*n/3
+    print("{} {}".format(n2, mesure))
