cf8aa5c3e527b30337ae9c42936aa6e34ea60736
Grifz2z <82527975+Grifz2z@users.noreply.github.com>
2021-06-21 19:54:35 +0200

    Update Puissance.py

--- Puissance.py
+++ Puissance.py
@@ -1,14 +1,16 @@
 n1 = int(input("Entrer le nombre à calculer : "))
 n2 = int(input("La puissance : "))
 if n2 > 0:
-    for n2 in range(n2):
-        n3=n1*n1
+    float(n2)
+    n3 = pow(n1, n2)
     print(n3)
-    #n3 = n1
-    #n3 = pow(n1,n2)
-    #print(n3)
+
 elif n2 < 0:
-    for n2 in range(n2):
-        n4=n1
+    n2 = -n2
+    n5=pow(n1, n2)
+    n4=1/n5
+    print(n4) 
+     
+
 else : 
-    print("ERREUR")
+    print("1")
