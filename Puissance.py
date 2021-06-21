3e8eb422baeb89c4c1ed570161db9f5582c4c293
Grifz2z <82527975+Grifz2z@users.noreply.github.com>
2021-06-21 19:10:52 +0200

    Update Puissance.py

--- Puissance.py
+++ Puissance.py
@@ -1,7 +1,7 @@
 n1 = int(input("Entrer le nombre à calculer: "))
 n2 = int(input("Entrer le nombre à calculer: "))
 if n2 < 0:
-n3 = n1
-n3 = pow(n1,n2)
-
-print(n3)
+    n3 = n1
+    n3 = pow(n1,n2)
+    print(n3)
+elif n2 > 0:
